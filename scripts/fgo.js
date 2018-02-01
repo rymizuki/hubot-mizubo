6010bd0cd34972ca64eff012157fc149c3aa3798
mizuki_r <ry.mizuki@gmail.com>
2018-02-02 08:42:43 +0900

    feat(fgo): drop情報追加

--- scripts/fgo.js
+++ scripts/fgo.js
@@ -4,38 +4,22 @@
 // Commands:
 //   hubot fgo 鯖 <名前> - サーヴァント情報を取得します
 //
-
-const axios = require('axios')
-const { template } = require('lodash')
-
-class ServantDatasource {
-  constructor () {
-    this.uriTmpl = template('https://kamigame.jp/fgo/<%= resource %>/<%= dataType %>/<%= name %>.html')
-    this.resource = '攻略データベース'
-    this.dataType = 'サーヴァント一覧'
-  }
-  findUri (name) {
-    const uri = this.uriTmpl({
-      resource: encodeURIComponent(this.resource),
-      dataType: encodeURIComponent(this.dataType),
-      name:     encodeURIComponent(name),
-    })
-    return axios.get(uri)
-      .then(() => {
-        return uri
-      })
-  }
-}
+const ServantDatasource = require('servant-datasource')
+const FgoDropEfficiency = require('fgo-drop-efficiency')
 
 module.exports = function (robot) {
   const servant = new ServantDatasource()
+  const dropEfficiency = new FgoDropEfficiency()
 
   function createHandler (msg, name) {
     return function handleError (err) {
       if (err.response.status == 404) {
         msg.send(`${ name }の情報は見つかりませんでした`)
-      } else {
+      } else if (err.response.status) {
         msg.send(`取得できません。ステータス ${ err.response.status }`)
+      } else {
+        robot.logger.error(err)
+        msg.send(`取得できません`)
       }
     }
   }
@@ -48,5 +32,40 @@ module.exports = function (robot) {
         msg.send(`${ name }の情報: ${ uri }`)
       })
       .catch(createHandler(msg, name))
+  })
+
+  function formatOutput (item) {
+    const header = `${ item.name }のドロップ情報:`
+    const contents = []
+    item.records.forEach(({ rank, area, quest, drop_rate, drop_rate_unit }) => {
+      contents.push(`${ rank } ${ area || '-' } ${ quest || '-' } ${ drop_rate || '-' }${ drop_rate_unit }`)
+    })
+    if (contents.length > 0) {
+      contents.unshift(header)
+    } else {
+      contents.pusth(header)
+      contents.push(`見つかりませんでした`)
+    }
+    return contents.join('\n')
+  }
+
+  robot.respond(/fgo drop (.+)/, (msg) => {
+    const name = msg.match[1]
+    return dropEfficiency.search(name)
+      .then((items) => {
+        robot.logger.debug('got items', items)
+        if (items.length == 0) {
+          msg.send(`${ name }の情報は見つかりませんでした`)
+          return
+        }
+        items.forEach((item) => {
+          const output = formatOutput(item)
+          msg.send(output)
+        })
+      })
+      .catch((err) => {
+        msg.send('取得できませんでした')
+        robot.logger.error(err)
+      })
   })
 }
