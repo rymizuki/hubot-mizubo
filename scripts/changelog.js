9be52431f9542eb9a47cba973c1e6a05b55d7667
mizuki_r <ry.mizuki@gmail.com>
2018-02-08 01:52:09 +0900

    fix(changelog): 対応できないフォーマットがあったので修正

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -20,7 +20,9 @@ function getReleases (raw) {
   let date
 
   rows.forEach((row) => {
-    if (/^#\s+([0-9]+\.[0-9]+\.[0-9]+)\s+(?:\(([0-9]{4}-[0-9]{2}-[0-9]{2})\))/.test(row)) {
+    const pattern_version_link = /^#\s+\[([0-9]+\.[0-9]+\.[0-9]+)\]\((?:.+)\)\s(?:\(([0-9]{4}-[0-9]{2}-[0-9]{2})\))/
+    const pattern_version      = /^#\s+([0-9]+\.[0-9]+\.[0-9]+)\s(?:\(([0-9]{4}-[0-9]{2}-[0-9]{2})\))/
+    if (pattern_version.test(row) || pattern_version_link.test(row)) {
       version = RegExp.$1
       date    = RegExp.$2
       releases[version] = {
@@ -40,7 +42,7 @@ function getRecentData (raw) {
   const version  = Object.keys(releases)[0]
   const release = releases[version]
 
-  if (release == null) return {}
+  if (release == null) return null
 
   return {
     title: `${ release.version } (${ release.date })`,
@@ -52,7 +54,14 @@ module.exports = function (robot) {
   robot.respond(/changelog/, function (msg) {
     const envelope = msg.envelope
     const raw = loadChangelog()
-    const { title, content } = getRecentData(raw)
+    const data = getRecentData(raw)
+
+    if (data == null) return msg.send('取得に失敗しました')
+
+    const { title, content } = data
+
+    robot.logger.debug('[changelog] title', title)
+    robot.logger.debug('[changelog] content', content)
 
     robot.upload(envelope, title, {
       title,
