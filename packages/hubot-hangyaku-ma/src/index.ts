f89bca83528a40168fa342e972a1f34b84bd8241
mizuki_r <ry.mizuki@gmail.com>
2019-03-11 22:23:51 +0900

    feat(hubot-hangyaku): remove

--- packages/hubot-hangyaku-ma/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { IRobot } from 'xhubot'
-import * as path from 'path'
-
-module.exports = function (robot: IRobot) {
-  const scriptPath = path.join(__dirname, ".");
-  robot.loadFile(scriptPath, "hubot.js");
-};
